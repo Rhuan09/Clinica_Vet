dfd4ae0db83452b5e85e6fecaf98b618a3f32b5c
Rhuan09 <rhuanalvesinocencio@gmail.com>
2024-11-25 12:32:04 -0300

    Ultimas Alteraçoes

--- Clinica_Vet/Views/HistoricoPage.xaml.cs
+++ Clinica_Vet/Views/HistoricoPage.xaml.cs
@@ -1,7 +1,9 @@
+using Clinica_Vet.DataAccess;
 using Clinica_Vet.ViewModels;
 using CommunityToolkit.Mvvm.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System;
 
 namespace Clinica_Vet.Views
 {
@@ -21,5 +23,38 @@ namespace Clinica_Vet.Views
                 await viewModel.CarregarHistoricoAsync();
             }
         }
+
+        private async void OnExcluirProdutoClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is ProdutoHistorico produtoHistorico)
+            {
+                // Confirmação do usuário
+                var dialog = new ContentDialog
+                {
+                    Title = "Confirmação de Exclusão",
+                    Content = $"Tem certeza de que deseja excluir o histórico do produto \"{produtoHistorico.Nome}\"?",
+                    PrimaryButtonText = "Sim",
+                    CloseButtonText = "Cancelar",
+                    DefaultButton = ContentDialogButton.Close,
+                    XamlRoot = this.XamlRoot
+                };
+
+                var result = await dialog.ShowAsync();
+                if (result == ContentDialogResult.Primary)
+                {
+                    if (DataContext is HistoricoViewModel viewModel)
+                    {
+                        // Exclui o item do banco de dados
+                        var produtoHistoricoDao = Ioc.Default.GetRequiredService<IDataAccess<ProdutoHistorico>>();
+                        await produtoHistoricoDao.RemoverAsync(produtoHistorico);
+
+                        // Atualiza a lista
+                        viewModel.ProdutosHistorico.Remove(produtoHistorico);
+                    }
+                }
+            }
+        }
+
+
     }
 }
